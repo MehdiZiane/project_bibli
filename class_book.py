0cc593f5eadf1f94b2f7662f6ee025230b8f9c01
seth111 <48691368+seth111@users.noreply.github.com>
2023-05-29 16:59:13 +0200

    ajout fichier class_book.py contenant la class 'book'

---
+++ class_book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, book_id, title, author, publication_year, isbn, category):
+        self.id = book_id
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.isbn = isbn
+        self.category = category
