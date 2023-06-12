b78a9337af2545271f1f3671a40785f1bc7e1102
seth111 <48691368+seth111@users.noreply.github.com>
2023-06-12 20:53:44 +0200

    import methods from homepage

--- classes/Admin_Page.py
+++ classes/Admin_Page.py
@@ -21,17 +21,6 @@ class user_page():
         self.frame_user_top = tkinter.Frame(self.window)
         self.frame_user_top.pack(side='top', padx=20, pady=20)
         
-        
-    
-
-
-class Admin_Page(user_page):
-    def __init__(self):
-        super().__init__()
-        self.window.title("Admin_Page")
-        self.window.configure(bg="#15c3f2")
-        
-        
     def open_book_detail_page(self, book):
         """ Function used to open a page giving details of a book 
 
@@ -44,6 +33,90 @@ class Admin_Page(user_page):
         self.frame_titre.destroy()
         book_details_page = BookDetailsPage(self.window, book, self.run_display, self.logged_in_user)
         book_details_page.run_display_book()
+    
+    def login(self):
+        """ Function enablling the user to connect to his user account in the library
+        """
+        dialog = tkinter.Toplevel()
+        dialog.title('Log In')
+
+        email_label = tkinter.Label(dialog, text='Email:')
+        email_label.pack()
+        email_entry = tkinter.Entry(dialog)
+        email_entry.pack()
+
+        password_label = tkinter.Label(dialog, text='Password:')
+        password_label.pack()
+        password_entry = tkinter.Entry(dialog, show='*')
+        password_entry.pack()
+
+        login_button = tkinter.Button(dialog, text='Log In', command=lambda: self.check_login(email_entry.get(), password_entry.get(), dialog))
+        login_button.pack(pady=10)
+        
+    def load_books(self):
+        try:
+            with open("./db/book.json", "r") as file:
+                data = json.load(file)
+                self.books = [Book(item['id'], item['titre'], item['auteur'], item['annee_publication'], item['isbn'], item['categorie']) for item in data]
+                print(self.books)
+        except FileNotFoundError:
+            self.books = []
+            
+    def display_login_signup(self):
+        """ Function that displays the library login and registration buttons
+        """
+        
+        self.frame_right = tkinter.Frame(self.window)
+        self.frame_right.pack(side = 'right', padx=20, pady=20)
+        
+        # Button Log In
+        log_in_button = tkinter.Button(self.frame_right, text='Log In', command=self.login)
+        log_in_button.pack(pady=10)
+
+        # Button Sign Up
+        sign_up_button = tkinter.Button(self.frame_right, text='Sign Up', command=self.create_account_dialog)
+        sign_up_button.pack(pady=10)
+        
+    def display_book(self):
+
+        self.frame_titre = tkinter.Frame(self.window)
+        self.frame_titre.pack(side='top', padx=20, pady=20)
+        titre = tkinter.Label(self.frame_titre, text="Bienvenue dans la bibliothèque", font=("Arial", 24))
+        titre.pack(pady=20)
+
+        # Creating the left frame for displaying books
+        self.frame_left = tkinter.Frame(self.window)
+        self.frame_left.pack(side='left', padx=20, pady=20)
+        
+        # Loading books from a JSON file
+        self.load_books()
+
+        # Loop over books and display them
+        for book in self.books:
+            button_text= f"{book.title} - {book.author}"
+            button = tkinter.Button(self.frame_left, text=button_text, command=lambda book=book: self.open_book_detail_page(book))
+            button.pack(pady=5)
+        
+        # Label which will receive information after the user logs in
+        self.user_label = tkinter.Label(self.window, text='', font=("Arial", 16))
+        self.user_label.pack(pady=10)
+        
+    def run_display(self):
+        self.display_book()
+        self.display_login_signup()
+        
+        
+    
+
+
+class Admin_Page(user_page):
+    def __init__(self):
+        super().__init__()
+        self.window.title("Admin_Page")
+        self.window.configure(bg="#15c3f2")
+        
+        
+   
     
         
         # # Affichage de la page
