ce97a4e183e79f785712e82af6241874dbcfcc28
MehdiZiane <mehdi.ziane@outlook.fr>
2023-06-13 05:47:16 +0200

    add all the methode usefull for the reserve

--- classes/book_details_page.py
+++ classes/book_details_page.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import json
+from tkinter import messagebox
 from classes.class_book import Book
 from classes.wishlist import Wishlist
 
@@ -21,20 +22,71 @@ class BookDetailsPage():
         self.frame_detail_right = tk.Frame(self.window)
         self.frame_detail_right.pack(side='right', padx=20, pady=20)
 
+        print(self.book.is_reserved)
+        print(self.book.reserved_by)
+
         self.display_book_details()
 
-    def load_books(self):
-        try:
-            with open("./db/book.json", "r") as file:
-                data = json.load(file)
-                self.book = [Book(item['id'], item['titre'], item['auteur'], item['annee_publication'], item['isbn'], item['categorie']) for item in data]
-        except FileNotFoundError:
-            self.book = []
+    def set_logged_in_user(self, user):
+        self.logged_in_user = user
+    
+    def load_database(self):
+        with open('./db/book.json', 'r') as file:
+            data = json.load(file)
+        return data
+    
+    def save_database(self, data):
+        with open('./db/book.json', 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def reserve(self):
+        if not self.book.is_reserved:
+            if self.logged_in_user:
+                self.book.is_reserved = True
+                self.book.reserved_by = self.logged_in_user['id']
+                self.update_reserve_cancel_buttons()
+                self.window.after(2 * 60 * 60 * 1000, self.cancel_reserve)  # Lancer le timer de 2 heures
+                messagebox.showinfo('info', 'livre reservé avec succes')
+                self.update_database()
+                print(self.book.is_reserved)
+                print(self.book.reserved_by)
+            else:
+                messagebox.showinfo('Info', 'Vous devez être connecté pour réserver un livre.')
+        else:
+            messagebox.showinfo('Info', 'Ce livre est déjà réservé.')
+    
+    def cancel_reserve(self):
+        # Annuler la réservation du livre
+        self.book.is_reserved = False
+        self.book.reserve_by = None
+        self.update_reserve_cancel_buttons()
+        messagebox.showinfo('Information', 'Réservation annulée avec succès.')
+        self.update_database()
+        print(self.book.is_reserved)
+        print(self.book.reserved_by)
+    
+    def update_reserve_cancel_buttons(self):
+        # Vérifier si le livre est réservé et mettre à jour les états des boutons en conséquence
+        if self.logged_in_user['id'] == self.book.reserved_by:
+            if self.book.is_reserved:
+                self.reserve_button.config(state=tk.DISABLED)  # Désactiver le bouton de réservation
+                self.cancel_button.config(state=tk.NORMAL)  # Activer le bouton d'annulation de réservation
+            else:
+                self.reserve_button.config(state=tk.NORMAL)  # Activer le bouton de réservation
+                self.cancel_button.config(state=tk.DISABLED)  # Désactiver le bouton d'annulation de réservation
+
+    def update_database(self):
+        data = self.load_database()
+        for book_data in data:
+            if book_data['id'] == self.book.book_id:
+                book_data['is_reserved'] = self.book.is_reserved
+                book_data['reserved_by'] = self.book.reserved_by
+                break
+        self.save_database(data)  
         
         # Créez les widgets et la mise en page pour la page des détails du livre ici
     def display_book_details(self):
         
-        self.load_books()
         
         # Titre du livre
         title_label = tk.Label(self.frame_detail_left, text="Titre: " + self.book[0].title)
@@ -61,11 +113,11 @@ class BookDetailsPage():
         back_button.pack()
         
         #Bouton pour réserver
-        reserve_button = tk.Button(self.frame_detail_right, text="reserve")
+        reserve_button = tk.Button(self.frame_detail_right, text="reserve", command=self.reserve)
         reserve_button.pack()
         
         #Bouton pour annuler une réservation
-        cancel_reservation_button = tk.Button(self.frame_detail_right, text="Cancel reservation")
+        cancel_reservation_button = tk.Button(self.frame_detail_right, text="Cancel reservation", command=self.cancel_reserve, state=tk.DISABLED)
         cancel_reservation_button.pack()
         
         #Bouton d'ajout à la liste de souhaits
